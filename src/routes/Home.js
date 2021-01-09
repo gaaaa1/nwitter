1d026af5ad510664e838fa7acc248496c3bad3d1
gaaaa1 <gusckdska123@naver.com>
2021-01-10 00:21:07 +0900

    try getting Nweet from firestore but error

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "../fBase";
 
-const Home = () => <span>Home</span>
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getNweets();
+    }, []);
+    const onSubmit = async (event) => {
+        event.preventDefault(); //a 태그나 submit 태그는 누르게 되면 href 를 통해 이동하거나, 창이 새로고침하여 실행됩니다. 이를 막아주는 부분
+        await dbService.collection("nweets").add({
+            nweet,
+            createdAt: Date.now(),
+        });
+        setNweet("");
+    };
+    const onChange = (event) => {
+        const { target: { value }, } = event; // event 안에있는 value를 달라는 의미
+        setNweet(value);
+    };
+    console.log(nweets);
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={nweet}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="what's on your mind?"
+                    maxLength={120} />
+                <input type="submit" value="Nweet" />
+            </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                    ))}
+            </div>
+        </div>
+    );
+};
 export default Home;
