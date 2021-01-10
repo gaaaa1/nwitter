b6d7c6406c996d68743f6d1c26ef38894ab2ca3d
gaaaa1 <gusckdska123@naver.com>
2021-01-10 10:28:10 +0900

    realtime nweets on firebasestore with snapshot

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,35 +1,36 @@
-import React, { useEffect, useState } from "react";
+
+import React, { useState, useEffect } from "react";
 import { dbService } from "../fBase";
 
-const Home = () => {
+const Home = ({ userObj }) => {
+
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-    const getNweets = async () => {
-        const dbNweets = await dbService.collection("nweets").get();
-        dbNweets.forEach((document) => {
-            const nweetObject = {
-                ...document.data(),
-                id: document.id,
-            };
-            setNweets((prev) => [nweetObject, ...prev]);
-        });
-    };
+    
     useEffect(() => {
-        getNweets();
+      
+        dbService.collection("nweets").onSnapshot(snapshot => {
+            const nweetArray = snapshot.docs.map(doc => ({
+                id: doc.id, ...doc.data(),
+            }));
+            setNweets(nweetArray);
+        });
     }, []);
     const onSubmit = async (event) => {
-        event.preventDefault(); //a 태그나 submit 태그는 누르게 되면 href 를 통해 이동하거나, 창이 새로고침하여 실행됩니다. 이를 막아주는 부분
+        event.preventDefault();
         await dbService.collection("nweets").add({
-            nweet,
+            text: nweet,
             createdAt: Date.now(),
+            creatorId: userObj.uid,
         });
         setNweet("");
     };
     const onChange = (event) => {
-        const { target: { value }, } = event; // event 안에있는 value를 달라는 의미
+        const {
+            target: { value },
+        } = event;
         setNweet(value);
     };
-    console.log(nweets);
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -37,16 +38,17 @@ const Home = () => {
                     value={nweet}
                     onChange={onChange}
                     type="text"
-                    placeholder="what's on your mind?"
-                    maxLength={120} />
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                />
                 <input type="submit" value="Nweet" />
             </form>
             <div>
                 {nweets.map((nweet) => (
-                    <div>
-                        <h4>{nweet.nweet}</h4>
+                    <div key={nweet.id}>
+                        <h4>{nweet.text}</h4>
                     </div>
-                    ))}
+                ))}
             </div>
         </div>
     );
