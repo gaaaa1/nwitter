211b04be1607fbc547d3da50d7e77589043253f9
gaaaa1 <gusckdska123@naver.com>
2021-02-13 01:12:52 +0900

    mod profile's name

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { authService, dbService } from "../fBase";
 
-export default ({ userObj }) => {
+export default ({ refreshUser, userObj }) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const getMyNweets = async () => {
         //쿼리 , 데이터베이스에서 userid로 입력된 데이터 가져오기 및 일자별로 정렬
         const nweets = await dbService.collection("nweets").where("creatorId", "==", userObj.uid)
@@ -22,15 +23,28 @@ export default ({ userObj }) => {
         } = event;
         setNewDisplayName(value);
     };
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                displayName: newDisplayName,
+
+            });
+            refreshUser();
+        }
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <input type="text" placeholder="Display name" />
-            <input type="submit" value="Update Profile"/>
-        </form>
+        <>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    type="text"
+                    placeholder="Display name"
+                    value={newDisplayName}
+                />
+                <input type="submit" value="Update Profile"/>
+            </form>
 
             <button onClick={ onLogOutClick }>Log Out</button>
         </>
